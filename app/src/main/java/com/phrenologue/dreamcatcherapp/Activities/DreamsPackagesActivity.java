9a4a0b2f0f23f2c542f7918f49454e23e76cce91
tikas <talee.eh@gmail.com>
2020-03-14 15:09:24 +0330

    AAAAAAAASHT!!!

--- app/src/main/java/com/phrenologue/dreamcatcherapp/Activities/DreamsPackagesActivity.java
+++ app/src/main/java/com/phrenologue/dreamcatcherapp/Activities/DreamsPackagesActivity.java
@@ -1,24 +1,15 @@
 package com.phrenologue.dreamcatcherapp.Activities;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.phrenologue.dreamcatcherapp.Activities.Adapter.DreamsPackagesActivityAdapter;
 import com.phrenologue.dreamcatcherapp.Ui.costumeFont.MoonTextView;
 import com.phrenologue.dreamcatcherapp.databinding.ActivityDreamsPackagesBinding;
-import com.phrenologue.dreamcatcherapp.parameters.IResponseMessage;
 import com.phrenologue.dreamcatcherapp.presenters.DreamsPresenter;
-import com.phrenologue.dreamcatcherapp.webservice.ApiPostCaller;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class DreamsPackagesActivity extends AppCompatActivity {
@@ -38,40 +29,8 @@ public class DreamsPackagesActivity extends AppCompatActivity {
         dreamsRecycler = binding.dreamsRecycler;
         dreamCount = binding.titleDreamsCount;
         dreamHours = binding.titleDreamHours;
-        ApiPostCaller postCaller = new ApiPostCaller();
-        postCaller.getDreamDescription(new IResponseMessage() {
-            @Override
-            public void onSuccess(Object response) throws JSONException {
-
-                JSONArray jsonArray = new JSONArray(response.toString());
-
-                ArrayList<JSONArray> jsonArrays = new ArrayList();
-
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    jsonArrays.add(jsonArray.getJSONArray(i));
-                }
-                titles = new ArrayList<String>();
-                contents = new ArrayList<String>();
-                Log.e("", "");
-                for (int j = 0; j < jsonArrays.size(); j++) {
-                    titles.add(jsonArrays.get(j).getString(0));
-                    contents.add(jsonArrays.get(j).getString(1));
-                }
-
-                DreamsPackagesActivityAdapter adapter = new DreamsPackagesActivityAdapter(getApplicationContext(), titles, contents);
-                RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
-                dreamsRecycler.setLayoutManager(layoutManager);
-                dreamsRecycler.setAdapter(adapter);
-                dreamCount.setText("Dream Count: "+titles.size()+"");
-            }
-
-            @Override
-            public void onFailure(String errorMessage) {
-
-            }
-
-        });
-
+        presenter = new DreamsPresenter(this);
+        presenter.getDescription(dreamsRecycler,dreamCount);
 
     }
 
--- app/src/main/java/com/phrenologue/dreamcatcherapp/presenters/DreamsPresenter.java
+++ app/src/main/java/com/phrenologue/dreamcatcherapp/presenters/DreamsPresenter.java
@@ -1,8 +1,21 @@
 package com.phrenologue.dreamcatcherapp.presenters;
 
 import android.content.Context;
+import android.util.Log;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.phrenologue.dreamcatcherapp.Activities.Adapter.DreamsPackagesActivityAdapter;
+import com.phrenologue.dreamcatcherapp.Ui.costumeFont.MoonTextView;
+import com.phrenologue.dreamcatcherapp.parameters.IResponseMessage;
+import com.phrenologue.dreamcatcherapp.webservice.ApiPostCaller;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DreamsPresenter {
     private Context context;
@@ -10,8 +23,42 @@ public class DreamsPresenter {
         this.context = context;
     }
 
-    public void getDescription(RecyclerView dreamRecycler){
+    public void getDescription(RecyclerView dreamsRecycler,
+                               MoonTextView dreamCount){
 
+        ApiPostCaller postCaller = new ApiPostCaller();
+        postCaller.getDreamDescription(new IResponseMessage() {
+            @Override
+            public void onSuccess(Object response) throws JSONException {
+
+                JSONArray jsonArray = new JSONArray(response.toString());
+
+                ArrayList<JSONArray> jsonArrays = new ArrayList();
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    jsonArrays.add(jsonArray.getJSONArray(i));
+                }
+                List<String> titles = new ArrayList<String>();
+                List<String> contents = new ArrayList<String>();
+                Log.e("", "");
+                for (int j = 0; j < jsonArrays.size(); j++) {
+                    titles.add(jsonArrays.get(j).getString(0));
+                    contents.add(jsonArrays.get(j).getString(1));
+                }
+
+                DreamsPackagesActivityAdapter adapter = new DreamsPackagesActivityAdapter(context, titles, contents);
+                RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(context);
+                dreamsRecycler.setLayoutManager(layoutManager);
+                dreamsRecycler.setAdapter(adapter);
+                dreamCount.setText("Dream Count: "+titles.size()+"");
+            }
+
+            @Override
+            public void onFailure(String errorMessage) {
+
+            }
+
+        });
 
     }
 }
