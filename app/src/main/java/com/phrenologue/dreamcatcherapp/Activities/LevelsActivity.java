4f0a32d01de495e16bd648e7d258b6fc2d41e68c
tikas <talee.eh@gmail.com>
2020-03-25 16:20:55 +0430

    AAAAAAAASHT!!!

--- app/src/main/java/com/phrenologue/dreamcatcherapp/Activities/LevelsActivity.java
+++ app/src/main/java/com/phrenologue/dreamcatcherapp/Activities/LevelsActivity.java
@@ -2,6 +2,7 @@ package com.phrenologue.dreamcatcherapp.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,6 +20,16 @@ public class LevelsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_levels);
         binding=ActivityLevelsBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        binding.btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
 
