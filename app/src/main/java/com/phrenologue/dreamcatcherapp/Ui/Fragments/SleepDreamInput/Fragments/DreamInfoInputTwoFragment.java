632f3342bf6d3dcb312340e06342c9300319f770
roozbeh <roozbeh.ghanbarzadeh1994@gmail.com>
2020-03-25 21:09:32 +0430

    animations added asht!

--- app/src/main/java/com/phrenologue/dreamcatcherapp/Ui/Fragments/SleepDreamInput/Fragments/DreamInfoInputTwoFragment.java
+++ app/src/main/java/com/phrenologue/dreamcatcherapp/Ui/Fragments/SleepDreamInput/Fragments/DreamInfoInputTwoFragment.java
@@ -1,5 +1,6 @@
 package com.phrenologue.dreamcatcherapp.Ui.Fragments.SleepDreamInput.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,14 +10,17 @@ import android.widget.Spinner;
 import androidx.appcompat.widget.AppCompatEditText;
 import androidx.fragment.app.Fragment;
 
-import com.phrenologue.dreamcatcherapp.Activities.EditDreamInputActivity;
+import com.phrenologue.dreamcatcherapp.Activities.DreamsPackagesActivity;
 import com.phrenologue.dreamcatcherapp.databinding.FragmentDreamInfoInputTwoBinding;
 import com.phrenologue.dreamcatcherapp.presenters.DreamInputPresenter;
+
+import maes.tech.intentanim.CustomIntent;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DreamInfoInputTwoFragment extends Fragment {
+
     DreamInputPresenter presenter;
     private FragmentDreamInfoInputTwoBinding binding;
     AppCompatEditText title, content;
@@ -48,6 +52,15 @@ public class DreamInfoInputTwoFragment extends Fragment {
             }
         });
 
+        binding.btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    Intent intent= new Intent(v.getContext(), DreamsPackagesActivity.class);
+                    startActivity(intent);
+                    CustomIntent.customType(getContext(), "fadein-to-fadeout");
+                }
+        });
 
         return view;
     }
