0e416be79e007eb5664761512766466f2a02f366
roozbeh <roozbeh.ghanbarzadeh1994@gmail.com>
2020-03-28 17:12:36 +0430

    animations added asht!

--- app/src/main/java/com/phrenologue/dreamcatcherapp/Activities/ProfileActivity.java
+++ app/src/main/java/com/phrenologue/dreamcatcherapp/Activities/ProfileActivity.java
@@ -142,7 +142,6 @@ public class ProfileActivity extends AppCompatActivity {
         Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
 
         new Handler().postDelayed(new Runnable() {
-
             @Override
             public void run() {
                 doubleBackToExitPressedOnce=false;
