8b5b71be5d7c5508da95aa41c1fccb3b0e9cfa1e
roozbeh <roozbeh.ghanbarzadeh1994@gmail.com>
2020-03-31 16:50:53 +0430

    animations added asht!

--- app/src/main/java/com/phrenologue/dreamcatcherapp/Ui/Fragments/SleepDreamInput/Fragments/QuestionTwoFragment.java
+++ app/src/main/java/com/phrenologue/dreamcatcherapp/Ui/Fragments/SleepDreamInput/Fragments/QuestionTwoFragment.java
@@ -1,13 +1,18 @@
 package com.phrenologue.dreamcatcherapp.Ui.Fragments.SleepDreamInput.Fragments;
 
+import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
-import com.phrenologue.dreamcatcherapp.R;
+import com.phrenologue.dreamcatcherapp.databinding.FragmentQuestionTwoBinding;
+
+import static android.text.Layout.JUSTIFICATION_MODE_INTER_WORD;
 
 
 /**
@@ -15,15 +20,25 @@ import com.phrenologue.dreamcatcherapp.R;
  */
 public class QuestionTwoFragment extends Fragment {
 
+    private FragmentQuestionTwoBinding binding;
+
     public QuestionTwoFragment() {
         // Required empty public constructor
     }
 
-
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_question_two, container, false);
+
+        binding = FragmentQuestionTwoBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        binding.questionTwo.setTypeface(Typeface.DEFAULT_BOLD);
+        binding.questionTwoTitle.setTypeface(Typeface.DEFAULT_BOLD);
+        binding.questionTwo.setJustificationMode(JUSTIFICATION_MODE_INTER_WORD);
+
+        return view;
+
     }
 }
